0c52e6149cec73c6cb9f67e530aa176088f547c4
Darkiller1234 <kyoung8292@naver.com>
2024-10-24 09:02:21 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
+import { Title, DescriptText } from './components/CommonsStyles';
+import SearchBar from './components/SearchBar';
+import { useState } from 'react';
+import { CallGpt, CallGptAxios } from './service/gptapi';
 
 function App() {
+  const [searchText, setSearchText] = useState("")
+
+  const changeSearchText = (ev) => {
+    setSearchText(ev.target.value);
+  }
+
+  const clickSearchIcon = async () => {
+    if(searchText.trim() === "")
+      return;
+    
+    const chatData = {
+      date: new Date(),
+      question: searchText
+    }
+
+    const message = await CallGptAxios({
+      prompt: searchText
+    })
+    console.log(message);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer className="App">
+      <Header>
+        <Title>나만의 GPT</Title>
+      </Header>
+      <Contents>
+
+      </Contents>
+      <Footer>
+        <SearchBar
+          searchText = {searchText}
+          changeSearchText = {changeSearchText}
+          clickSearchIcon = {clickSearchIcon}
+        />
+        <DescriptText>
+          ChatGPT는 실수를 할 수 있습니다. 중요한 정보를 확인하세요.
+        </DescriptText>
+      </Footer>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100%;
+    max-width: 720px;
+    margin: 0 auto;
+`
+
+const Header = styled.div`
+    padding: 8px;
+    height: 56px;
+    width: 100%;
+    background: white;
+`
+
+const Contents = styled.div`
+    padding: 60px 0 0 0;
+    flex: 1;
+    overflow-y: scroll;
+`
+
+const Footer = styled.div`
+    height: 86px;
+    display: flex;
+    flex-direction: column;
+`
